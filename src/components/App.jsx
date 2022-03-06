179c6f7f0d83a23e251fcc89e10a2078213c3597
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-03-07 01:34:28 +0530

    [UPDATE] Form Psuedo COde Added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -46,4 +46,27 @@ function App() {
   );
 }
 
+// Form Handling Code 
+
+// function handleClick() {
+//   setHeading(name)
+
+//   event.preventDefault();
+// }
+
+// return (
+//   <div className="container">
+//     <h1>Hello {headinText}</h1>
+//     <form onSubmit={handleClick}>
+//       <input
+//         onChange={handleChange}
+//         type="text"
+//         placeholder="What's your name?"
+//         value={name}
+//       />
+//       <button type="submit">Submit</button>
+//     </form>
+//   </div>
+// )
+
 export default App;
