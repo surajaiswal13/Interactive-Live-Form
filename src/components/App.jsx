a740a13d25c1df6de4b1fc3cd30ff6d8631f0524
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-03-07 01:33:10 +0530

    [UPDATE] Interactive Input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [heading, setHeadingText] = useState("Hello");
+
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const [name, setName] = useState("");
+
+  function updateHeading() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouseOver() {
+    setIsMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setIsMouseOver(false);
+  }
+
+  function handleOnChange(event) {
+    setName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>
+        {heading} {name}
+      </h1>
+      <input
+        onChange={handleOnChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        onClick={updateHeading}
+      >
+        Submit
+      </button>
     </div>
   );
 }
